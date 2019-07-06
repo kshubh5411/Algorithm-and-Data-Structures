3b519157132bb9c47fafdce738519f40454d85cb
Shubham kumar <shubhamk541@gmail.com>
2019-07-06 22:02:59 +0530

    Update Subarray having sum-k.cpp

--- Array/Subarray having sum-k.cpp
+++ Array/Subarray having sum-k.cpp
@@ -1,1 +1,20 @@
 #include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+ cin>>n;
+ int a[n];
+ for(int i=0;i<n;i++)cin>>a[i];
+ int k;cin>>k;
+ unordered_map<int,int>mp;
+ mp.insert(make_pair(0,1));
+ int prev_sum=0,c=0;
+ for(int i=0;i<n;i++){
+     prev_sum+=a[i];
+     if(mp.find(prev_sum-k)!=mp.end()){
+       c+=mp[prev_sum-k];
+     }
+     mp[prev_sum]++;
+ }
+ cout<<c<<endl;
+}
